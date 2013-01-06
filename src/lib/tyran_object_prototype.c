27a62864f2701bdb7962d8ec757417465a86cd9c
Piot <piot@hotmail.com>
2013-01-06 12:24:48 +0100

    Removed old code

--- src/lib/tyran_object_prototype.c
+++ src/lib/tyran_object_prototype.c
@@ -3,21 +3,6 @@
 #include <tyranscript/tyran_runtime.h>
 #include <tyranscript/tyran_value.h>
 #include <tyranscript/tyran_value_object.h>
-/*
-
-#include <tyranscript/parser/tyran_parser_state.h>
-#include <tyranscript/tyran_value.h>
-#include <tyranscript/tyran_string.h>
-#include <tyranscript/tyran_function.h>
-
-#include <tyranscript/tyran_object_prototype.h>
-#include <tyranscript/tyran_function_prototype.h>
-
-#include <tyranscript/tyran_value_object.h>
-#include <tyranscript/tyran_object.h>
-#include <tyranscript/tyran_object_macros.h>
-#include <tyranscript/tyran_array.h>
-*/
 
 TYRAN_RUNTIME_CALL_FUNC(tyran_object_prototype_constructor) {
 	return 0;
