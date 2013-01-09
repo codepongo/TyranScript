8ecf70828b639895ebbaa685c61e2f8af9b1b865
Piot <piot@hotmail.com>
2013-01-09 01:03:23 +0100

    Fixed warning

--- src/lib/debug/mocha/tyran_mocha_parser_debug.c
+++ src/lib/debug/mocha/tyran_mocha_parser_debug.c
@@ -3,7 +3,7 @@
 void tyran_mocha_parser_node_print_tree(tyran_mocha_parser* parser, const char* description, tyran_parser_node* node)
 {
 	NODE* root = 0;
-	TYRAN_LOG("");
+	TYRAN_LOG("---");
 	TYRAN_LOG("*** %s ***", description);
 	root = parser->root;
 	tyran_parser_node_print_helper("", root, root, parser->next_node_to_overwrite, 0);
